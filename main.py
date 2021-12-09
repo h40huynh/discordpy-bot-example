816c5e376f86827d43d4dc9c8664ff19afbbb960
Hào Huỳnh <46048038+h40huynh@users.noreply.github.com>
2021-12-09 22:53:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from discordbot.bot import DiscordBOT
 from discordbot.cog.ping import Ping
 
-BOT_TOKEN = "OTE3NzQ2MjIyMzk4MzEyNDY4.Ya9Lxg.76QRuagbLSxe4fE7tivWt5I-ijY"
+BOT_TOKEN = ""
 LOGGING_CHANNEL = 918300079880822855
 
 bot = DiscordBOT(BOT_TOKEN, logging_channel_id=LOGGING_CHANNEL)
